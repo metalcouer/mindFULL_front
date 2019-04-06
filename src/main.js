b7a744b35518741d1f514676f8091346ca878118
Diane Sanchez <diane@Dianes-MacBook-Air-2.local>
2019-04-06 17:09:30 -0600

    added nav bar components'

--- src/main.js
+++ src/main.js
@@ -2,7 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueMq from 'vue-mq'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
+
+Vue.use(VueMq, {
+  breakpoints: {
+    mobile: 450,
+    tablet: 900,
+    laptop: 1250,
+    desktop: Infinity
+  }
+})
 Vue.config.productionTip = false
 
 new Vue({
